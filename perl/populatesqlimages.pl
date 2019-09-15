8d1c6a86246ddedc1ddab3d601178fe44884d679
chris <chris@cwiering-linux1.umflint.edu>
2019-09-15 15:11:08 -0400

    setupdocs/ORACLE_DB_Setup.txt - added ORDImage table and appropriate setup/procedures perl/populatesqlimages.pl - updated to populate captions/licenses

--- perl/populatesqlimages.pl
+++ perl/populatesqlimages.pl
@@ -21,7 +21,6 @@ my $jsonhash = decode_json $jsonin;
 
 # loop through all the images; check if there, if NOT, then populate it
 foreach my $image (@{$jsonhash->{'images'}}) {
-  print Dumper($image);
   # check to see if this one is populated already
   my $sth = $dbh->prepare('SELECT COUNT(*) AS NUMRECORDS FROM IMAGE WHERE ID=?');
   $sth->execute($image->{'id'});
@@ -37,9 +36,49 @@ foreach my $image (@{$jsonhash->{'images'}}) {
     $image->{'coco_url'},$image->{'date_captured'});
   
   # echo out printed
-  printf("INSERTED id = %d\n",$image->{'id'});
+  print Dumper($image);
+  printf("INSERTED image id = %d\n",$image->{'id'});
 }
+
+# loop through all the annotations/captions; check if there, if NOT, then populate it
+foreach my $caption (@{$jsonhash->{'annotations'}}) {
+  # check to see if this one is populated already
+  my $sth = $dbh->prepare('SELECT COUNT(*) AS NUMRECORDS FROM CAPTION WHERE ID=?');
+  $sth->execute($caption->{'id'});
+
+  # fetch it
+  my $ref = $sth->fetchrow_hashref();
+  if(defined($ref->{'NUMRECORDS'}) && $ref->{'NUMRECORDS'} >= 1) { next; }
+
+  # no records, so insert it
+  my $sth = $dbh->prepare("INSERT INTO CAPTION VALUES(?,?,?)");
+  $sth->execute($caption->{'id'},$caption->{'image_id'},$caption->{'caption'});
+  
+  # echo out printed
+  print Dumper($caption);
+  printf("INSERTED caption id = %d\n",$caption->{'id'});
+}
+
+# populate licenses
+# loop through all the annotations/captions; check if there, if NOT, then populate it
+foreach my $license (@{$jsonhash->{'licenses'}}) {
+  # check to see if this one is populated already
+  my $sth = $dbh->prepare('SELECT COUNT(*) AS NUMRECORDS FROM LICENSE WHERE ID=?');
+  $sth->execute($license->{'id'});
+
+  # fetch it
+  my $ref = $sth->fetchrow_hashref();
+  if(defined($ref->{'NUMRECORDS'}) && $ref->{'NUMRECORDS'} >= 1) { next; }
+
+  # no records, so insert it
+  my $sth = $dbh->prepare("INSERT INTO LICENSE VALUES(?,?,?)");
+  $sth->execute($license->{'id'},$license->{'name'},$license->{'url'});
+ 
+  # echo out printed
+  print Dumper($license);
+  printf("INSERTED license id = %d\n",$license->{'id'});
+}
+
 
 # exit out
 exit;
-
